26bae9c89662dce840aba8fb024e2309776771f7
roaddee <roaddee@gmail.com>
2020-05-14 19:39:32 +0700

    edit _js.php

--- application/views/_layout/_js.php
+++ application/views/_layout/_js.php
@@ -5,6 +5,13 @@
 <script src="<?php echo base_url(); ?>assets/plugins/iCheck/icheck.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!--Input Mask -->
+<!-- <script src="<?php echo base_url(); ?>assets/plugins/inputmask/jquery.inputmask.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/inputmask/jquery.inputmask.date.extension.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/inputmask/jquery.inputmask.extension.js"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+-->
+<script src="https://rawgit.com/RobinHerbots/jquery.inputmask/3.x/dist/jquery.inputmask.bundle.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url(); ?>assets/dist/js/app.min.js"></script>
 
