306a47c781e9b6da6cd526c62ee8335d7b53b89e
roaddee <roaddee@gmail.com>
2020-05-16 03:53:17 +0700

    rapikan model jasa

--- application/models/M_jasa.php
+++ application/models/M_jasa.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_jasa extends CI_Model {
+	
 	public function select_all() {
 		$this->db->select('*');
 		$this->db->from('jasa');
