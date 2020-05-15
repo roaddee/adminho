0ea0801f9d3c5bf9cc752257103cc38f5c4a860d
roaddee <roaddee@gmail.com>
2020-05-15 11:22:36 +0700

    penyesuaian struktur database

--- database/adminho.sql
+++ database/adminho.sql
@@ -1,191 +1,107 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 24 Feb 2017 pada 15.57
--- Versi Server: 10.1.13-MariaDB
--- PHP Version: 7.0.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `cendana`
---
-
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               10.4.12-MariaDB-log - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5669
 -- --------------------------------------------------------
 
---
--- Struktur dari tabel `admin`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
+-- Dumping structure for table adminho.admin
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(15) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `nama` varchar(50) DEFAULT NULL,
-  `foto` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `foto` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `admin`
---
-
+-- Dumping data for table adminho.admin: ~1 rows (approximately)
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 INSERT INTO `admin` (`id`, `username`, `password`, `nama`, `foto`) VALUES
-(1, 'auwfar', 'f0a047143d1da15b630c73f0256d5db0', 'Achmad Chadil Auwfar', 'Koala.jpg'),
-(2, 'ozil', 'f4e404c7f815fc68e7ce8e3c2e61e347', 'Mesut ', 'profil2.jpg');
+	(1, 'opendesa', '2696554fc2fd8c48d5d4148597096dc4', 'OpenDESA', 'logo-opendesa1.png');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 
--- --------------------------------------------------------
+-- Dumping structure for table adminho.jasa
+CREATE TABLE IF NOT EXISTS `jasa` (
+  `id` int(1) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
---
--- Struktur dari tabel `kelamin`
---
+-- Dumping data for table adminho.jasa: ~6 rows (approximately)
+/*!40000 ALTER TABLE `jasa` DISABLE KEYS */;
+INSERT INTO `jasa` (`id`, `nama`) VALUES
+	(1, 'Lengkap'),
+	(2, 'Update Saja'),
+	(3, 'Install Saja'),
+	(4, 'Hosting Saja'),
+	(5, 'Domain Saja'),
+	(6, 'Hosting+Domain');
+/*!40000 ALTER TABLE `jasa` ENABLE KEYS */;
 
-CREATE TABLE `kelamin` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Dumping structure for table adminho.pelaksana
+CREATE TABLE IF NOT EXISTS `pelaksana` (
+  `id` int(1) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `kelamin`
---
+-- Dumping data for table adminho.pelaksana: ~3 rows (approximately)
+/*!40000 ALTER TABLE `pelaksana` DISABLE KEYS */;
+INSERT INTO `pelaksana` (`id`, `nama`) VALUES
+	(1, 'Rudi Purwanto'),
+	(2, 'Herry Wanda'),
+	(3, 'Muhammad Ihsan');
+/*!40000 ALTER TABLE `pelaksana` ENABLE KEYS */;
 
-INSERT INTO `kelamin` (`id`, `nama`) VALUES
-(1, 'Laki laki'),
-(2, 'Perempuan');
+-- Dumping structure for table adminho.pelanggan
+CREATE TABLE IF NOT EXISTS `pelanggan` (
+  `id` varchar(255) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `kecamatan` varchar(50) DEFAULT NULL,
+  `kabupaten` varchar(50) DEFAULT NULL,
+  `provinsi` varchar(50) DEFAULT NULL,
+  `namakontak` varchar(50) DEFAULT NULL,
+  `nomorkontak` varchar(50) DEFAULT NULL,
+  `domain` varchar(50) DEFAULT NULL,
+  `alamat_cpanel` varchar(50) DEFAULT NULL,
+  `uname_cpanel` varchar(50) DEFAULT NULL,
+  `pwd_cpanel` varchar(50) DEFAULT NULL,
+  `pwd_admin` varchar(50) DEFAULT NULL,
+  `id_jasa` int(1) DEFAULT NULL,
+  `rupiah` varchar(50) DEFAULT NULL,
+  `tgl_mulai` date DEFAULT NULL,
+  `tgl_akhir` date DEFAULT NULL,
+  `id_pelaksana` int(1) DEFAULT NULL,
+  `tempat_hosting` int(1) DEFAULT NULL,
+  `keterangan` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
--- --------------------------------------------------------
+-- Dumping data for table adminho.pelanggan: ~0 rows (approximately)
+/*!40000 ALTER TABLE `pelanggan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pelanggan` ENABLE KEYS */;
 
---
--- Struktur dari tabel `kota`
---
-
-CREATE TABLE `kota` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `kota`
---
-
-INSERT INTO `kota` (`id`, `nama`) VALUES
-(1, 'Malang'),
-(3, 'Blitar'),
-(4, 'Tulungagung'),
-(17, 'Jakarta'),
-(21, 'Surabaya'),
-(22, 'Paris');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pegawai`
---
-
-CREATE TABLE `pegawai` (
+-- Dumping structure for table adminho.pelanggan1
+CREATE TABLE IF NOT EXISTS `pelanggan1` (
   `id` varchar(255) NOT NULL,
   `nama` varchar(255) DEFAULT NULL,
   `telp` varchar(255) DEFAULT NULL,
-  `id_kota` int(11) DEFAULT NULL,
+  `id_jasa` int(11) DEFAULT NULL,
   `id_kelamin` int(1) DEFAULT NULL,
   `id_pelaksana` int(11) DEFAULT NULL,
   `status` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `pegawai`
---
+-- Dumping data for table adminho.pelanggan1: ~0 rows (approximately)
+/*!40000 ALTER TABLE `pelanggan1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pelanggan1` ENABLE KEYS */;
 
-INSERT INTO `pegawai` (`id`, `nama`, `telp`, `id_kota`, `id_kelamin`, `id_posisi`, `status`) VALUES
-('10', 'Antony Febriansyah Hartono', '082199568540', 1, 1, 1, 1),
-('11', 'Hafizh Asrofil Al Banna', '087859615271', 1, 1, 1, 1),
-('12', 'Faiq Fajrullah', '085736333728', 1, 1, 2, 1),
-('3', 'Mustofa Halim', '081330493322', 1, 1, 3, 1),
-('4', 'Dody Ahmad Kusuma Jaya', '083854520015', 1, 1, 2, 1),
-('5', 'Mokhammad Choirul Ikhsan', '085749535400', 3, 1, 2, 1),
-('7', 'Achmad Chadil Auwfar', '08984119934', 2, 1, 1, 1),
-('8', 'Rizal Ferdian', '087777284179', 1, 1, 3, 1),
-('9', 'Redika Angga Pratama', '083834657395', 1, 1, 3, 1),
-('1', 'Tolkha Hasan', '081233072122', 1, 1, 4, 1),
-('2', 'Wawan Dwi Prasetyo', '085745966707', 4, 1, 4, 1);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `posisi`
---
-
-CREATE TABLE `posisi` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `posisi`
---
-
-INSERT INTO `posisi` (`id`, `nama`) VALUES
-(1, 'IT'),
-(2, 'HRD'),
-(3, 'Keuangan'),
-(4, 'Produk'),
-(5, 'Web Developer');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `kelamin`
---
-ALTER TABLE `kelamin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `kota`
---
-ALTER TABLE `kota`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `posisi`
---
-ALTER TABLE `posisi`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `kota`
---
-ALTER TABLE `kota`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
---
--- AUTO_INCREMENT for table `posisi`
---
-ALTER TABLE `posisi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
